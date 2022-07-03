60330665b8adf93163d42970b1836fdd0c20e596
Paul Lan <paul.lan@oracle.com>
2022-07-02 21:19:03 -0700

    Remove OIC

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,12 +1,11 @@
 import { h } from "preact";
 
-// import { OjOicSchemaCurator } from '../oj-oic-schema-curator/loader';
 import Counter from '../counter/counter';
 
 export function Content() {
   return (
     <div class="oj-web-applayout-max-width oj-web-applayout-content">
-      <Counter name="Preact Function COMP for OIC" />
+      <Counter name="Preact Function Component for Oracle JET, with Redux/Styled-Component." />
     </div>
   );
 };
